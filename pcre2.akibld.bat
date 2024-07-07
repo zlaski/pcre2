f026a337e61c8ba7390e9d1e3fe04c2b1672f559
Ziemowit Łąski <zlaski@ziemas.net>
2024-07-06 19:13:16 -0700

    Check in pcre2.akibld.bat
    Hopefully also resolve some line-ending issues

---
+++ pcre2.akibld.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+set "EVENT=%1"
+
+if /i "%EVENT%" == "PreCleanStep" (
+    echo PreCleanStep
+    exit /b 0
+)
+
+if /i "%EVENT%" == "PreBuildEvent" (
+    echo PreBuildEvent
+    copy /y %ProjectDir%\src\config.h.generic %IntDir%\config.h
+    copy /y %ProjectDir%\src\pcre2.h.generic %IntDir%\pcre2.h
+
+    exit /b 0
+)
+
+if /i "%EVENT%" == "PreLinkEvent" (
+    echo PreLinkEvent
+    exit /b 0
+)
+
+if /i "%EVENT%" == "PostBuildEvent" (
+    echo PostBuildEvent
+    exit /b 0
+)
+
+if /i "%EVENT%" == "CustomBuildStep" (
+    echo CustomBuildStep
+    exit /b 0
+)
+
+if /i "%EVENT%" == "TestingStep" (
+    echo TestingStep
+    exit /b 0
+)
+
+echo ************** INVALID BUILD EVENT: %EVENT% ********************
+exit /b 3
