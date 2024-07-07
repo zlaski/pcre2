37c0f23b573a567c97b9330034d634c00b37673e
Ziemowit Łąski <zlaski@ziemas.net>
2024-07-06 19:19:16 -0700

    Get rid of the pre/post files

--- pcre2.post.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal EnableExtensions EnableDelayedExpansion
-
-call "%AKIBLD%\build-event-env.bat" post %0 %*
-
-:: pcre2.vcxproj post-build
-
-:: msgbox "%OutDir%\%TargetFileName%"
- 
